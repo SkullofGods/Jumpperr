9e9dec9ef416e409381aa413b534c26a645d2484
SkullofGods <salahtinov.ilua2010.ru@gmail.com>
2022-10-25 22:08:37 +0300

    done creepers mechanic

--- Assets/Scripts/LevelMechanics/Wind.cs
+++ Assets/Scripts/LevelMechanics/Wind.cs
@@ -9,13 +9,11 @@ public class Wind : MonoBehaviour
     public Vector3 direction;
    private void OnTriggerEnter(Collider other)
    {
-       if (other.gameObject.CompareTag("Player"))
-           other.GetComponent<ConstantForce>().force= direction*force;
+       other.GetComponent<ConstantForce>().force= direction*force;
    }
 
    private void OnTriggerExit(Collider other)
    {
-       if (other.gameObject.CompareTag("Player"))
-           other.GetComponent<ConstantForce>().force = Vector3.zero;
+       other.GetComponent<ConstantForce>().force = Vector3.zero;
    }
 }
